0f82a49d32e9fd2c888820bf590088087ec6a7d5
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-14 22:40:13 +0300

    added blog route

---
+++ routes/blog.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router  = express.Router()
+const mysql = require('mysql2/promise')
+
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'systemablog'
+}
+
+router.get('/',async (req, res) => {
+  const connection = await mysql.createConnection(dbConfig)
+  const getAllBlogsSql = `SELECT * FROM blog`
+  const [rows] = await connection.query(getAllBlogsSql)
+  if (rows.length === 0) {
+    connection.end()
+    return res.status(400).json("No blog exists currently")
+  }
+
+  connection.end()
+  return res.status(200).json(rows)
+})
+
+
+router.get('/:id', async (req, res) => {
+    const {id} = req.params
+
+    const connection = await mysql.createConnection(dbConfig)
+    const getSelectedBlogSql = `SELECT * FROM blog WHERE id=?`
+    const [rows] = await connection.query(getSelectedBlogSql, [parseInt(id)])
+    if (rows.length === 0) {
+        connection.end()
+        return res.status(200).json("Blog with the selected id does not exist")
+    }
+
+    connection.end()
+    return res.status(200).json(rows)
+})
+
+module.exports = router
