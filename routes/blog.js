16efa41e7e57ab616fbb063dc53c75d4e326f2a3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-14 23:40:17 +0300

    added try catch block

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router  = express.Router()
+const router = express.Router()
 const mysql = require('mysql2/promise')
 
 const dbConfig = {
@@ -9,33 +9,38 @@ const dbConfig = {
     database: 'systemablog'
 }
 
-router.get('/',async (req, res) => {
-  const connection = await mysql.createConnection(dbConfig)
-  const getAllBlogsSql = `SELECT * FROM blog`
-  const [rows] = await connection.query(getAllBlogsSql)
-  if (rows.length === 0) {
-    connection.end()
-    return res.status(400).json("No blog exists currently")
-  }
-
-  connection.end()
-  return res.status(200).json(rows)
+router.get('/', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig)
+        const getAllBlogsSql = `SELECT * FROM blog`
+        const [rows] = await connection.query(getAllBlogsSql)
+        if (rows.length === 0) {
+            connection.end()
+            return res.status(400).json("No blog exists currently")
+        }
+        connection.end()
+        return res.status(200).json(rows)
+    } catch (error) {
+        return res.status(400).json(error)
+    }
 })
 
 
 router.get('/:id', async (req, res) => {
-    const {id} = req.params
-
-    const connection = await mysql.createConnection(dbConfig)
-    const getSelectedBlogSql = `SELECT * FROM blog WHERE id=?`
-    const [rows] = await connection.query(getSelectedBlogSql, [parseInt(id)])
-    if (rows.length === 0) {
+    const { id } = req.params
+    try {
+        const connection = await mysql.createConnection(dbConfig)
+        const getSelectedBlogSql = `SELECT * FROM blog WHERE id=?`
+        const [rows] = await connection.query(getSelectedBlogSql, [parseInt(id)])
+        if (rows.length === 0) {
+            connection.end()
+            return res.status(200).json("Blog with the selected id does not exist")
+        }
         connection.end()
-        return res.status(200).json("Blog with the selected id does not exist")
+        return res.status(200).json(rows)
+    } catch (error) {
+        return res.status(400).json(error)
     }
-
-    connection.end()
-    return res.status(200).json(rows)
 })
 
 module.exports = router
