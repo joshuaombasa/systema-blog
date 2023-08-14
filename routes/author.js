fa1876793b96c1998d3a791638b95c7cfabbe129
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-14 23:40:07 +0300

    added try catch block

--- routes/author.js
+++ routes/author.js
@@ -10,34 +10,39 @@ const dbConfig = {
 }
 
 router.get('/', async (req, res) => {
-    const connection = await mysql.createConnection(dbConfig)
-    const getAllAuthorsSql = `SELECT first_name, last_name FROM author`
-    const [rows] = await connection.query(getAllAuthorsSql)
+    try {
+        const connection = await mysql.createConnection(dbConfig)
+        const getAllAuthorsSql = `SELECT first_name, last_name FROM author`
+        const [rows] = await connection.query(getAllAuthorsSql)
 
-    if (rows.length === 0) {
+        if (rows.length === 0) {
+            connection.end()
+            return res.status(400).json("No authors currently exist in the database")
+        }
         connection.end()
-        return res.status(400).json("No authors currently exist in the database")
+        return res.status(200).json(rows)
+    } catch (error) {
+        res.status(400).json(error)
     }
-
-    connection.end()
-    return res.status(200).json(rows)
 
 })
 
 router.get('/:id', async (req, res) => {
-    const {id} = req.params
-    const connection = await mysql.createConnection(dbConfig)
-    const getSelectedAuthorSql = `SELECT first_name, last_name FROM author WHERE id=?`
-    const [rows] = await connection.query(getSelectedAuthorSql, [parseInt(id)])
+    try {
+        const { id } = req.params
+        const connection = await mysql.createConnection(dbConfig)
+        const getSelectedAuthorSql = `SELECT first_name, last_name FROM author WHERE id=?`
+        const [rows] = await connection.query(getSelectedAuthorSql, [parseInt(id)])
 
-    if (rows.length === 0) {
+        if (rows.length === 0) {
+            connection.end()
+            return res.status(400).json("Selected author not found")
+        }
         connection.end()
-        return res.status(400).json("Selected author not found")
+        return res.status(200).json(rows)
+    } catch (error) {
+        res.status(400).json(error)
     }
-
-    connection.end()
-
-    return res.status(200).json(rows)
 })
 
 
