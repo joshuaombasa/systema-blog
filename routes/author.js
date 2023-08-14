a1703fe261882a7ac221e01fb14875353ddcd232
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-14 23:29:23 +0300

    added endpoint to get one author

--- routes/author.js
+++ routes/author.js
@@ -24,5 +24,21 @@ router.get('/', async (req, res) => {
 
 })
 
+router.get('/:id', async (req, res) => {
+    const {id} = req.params
+    const connection = await mysql.createConnection(dbConfig)
+    const getSelectedAuthorSql = `SELECT first_name, last_name FROM author WHERE id=?`
+    const [rows] = await connection.query(getSelectedAuthorSql, [parseInt(id)])
+
+    if (rows.length === 0) {
+        connection.end()
+        return res.status(400).json("Selected author not found")
+    }
+
+    connection.end()
+
+    return res.status(200).json(rows)
+})
+
 
 module.exports = router
