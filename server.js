97d2f4fe5c5074e72cb40340f632059bd5c66654
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-14 23:17:18 +0300

    added authors route

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express()
 const port = 3000
 
 const blogRoutes = require('./routes/blog')
+const authorRoutes = require('./routes/author')
 
 app.use('/blogs', blogRoutes)
+app.use('/authors', authorRoutes)
 
 app.listen(port, () => {
     console.log(`server listening on port ${port}`)
