9f560508e6d2b6b45fbc3daa9b40e3ab0162f380
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-15 22:24:33 +0300

    updated signup route

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 
-app.use(cors)
+app.use(cors())
 
 app.use(express.json())
 
 const blogRoutes = require('./routes/blog')
 const authorRoutes = require('./routes/author')
+const loginRoutes = require('./routes/login')
+const signupRoutes = require('./routes/signup')
+
 
 app.use('/blogs', blogRoutes)
 app.use('/authors', authorRoutes)
+app.use('/login', loginRoutes)
+app.use('/signup', signupRoutes)
 
 app.listen(port, () => {
     console.log(`server listening on port ${port}`)
