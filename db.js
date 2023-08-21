77ae3a11e2c2ca7806f379ad9496cc893a588be6
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-21 21:17:35 +0300

    added mysql database config file

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise')
+
+
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'systemablog'
+}
+
+export const connection = await mysql.createConnection(dbConfig)
+
