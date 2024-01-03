25cfa08eae9accbb1b165edd4a3b7b854562a0a8
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-03 19:33:10 +0300

    update auth logic

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,11 +4,14 @@ const SECRET_KEY = '23DH2029E3E7D02'
 
 module.exports = async (req, res, next) => {
     const token = req.header('Authorization')
+
     if (!token) {
         return res.status(400).json("Please login first")
     }
 
     const decoded = await JWT.verify(token, SECRET_KEY)
     const userId = decoded.userId
+    req.userId = userId
+    console.log(userId)
     next()
 }
